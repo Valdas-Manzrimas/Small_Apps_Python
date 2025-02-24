948733aec859d3a9dde1618a95b2a2efff27fda9
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-02-24 17:27:45 +0100

    Thread and Client connection

--- Server.py
+++ Server.py
@@ -1,16 +1,26 @@
 import socket 
+from threading import Thread #Allows multiple threads to run at the same time
 
 host='localhost'
 port=8080
 
+clients={}
+addresses={}
+
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM) # SOCK_STREAM = TCP ; SOCK_DGRAM = UDP
 sock.bind((host,port))
 
-sock.listen(1)
-print('The server is running and listening to client request')
-conn,address=sock.accept()
+def accept_clients_connections(): 
+    while True:
+        client_conn,client_address=sock.accept()
+        print(client_address, 'Has connected')
+        client_conn.send('Welcome to the chat'.encode('utf-8'))
+        addresses[client_conn]=client_address
 
-message = 'You received important message'
-conn.send(message.encode())
+if __name__ == "__main__":
+    sock.listen(5)
+    print("Server is running")
 
-conn.close()
+    t1=Thread(target=accept_clients_connections)
+    t1.start()
+    t1.join() #Waits for the thread to finish
