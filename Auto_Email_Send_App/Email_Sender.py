2b2aa8dabee4a6a45c9f0cd95bceff17374aca26
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-02-25 18:05:56 +0100

    #2 some bug fixes

--- Auto_Email_Send_App/Email_Sender.py
+++ Auto_Email_Send_App/Email_Sender.py
@@ -5,7 +5,7 @@ import re
 def login():
     if validate_login():
         global username
-        global password # global only for learning purposes
+        global password
         username = str(entry1.get())
         password = str(entry2.get())
         global server
@@ -16,19 +16,19 @@ def login():
         server.starttls() # make connection secure
         server.login(username, password)
 
-        f2.pack()
+        f2.grid()
         btn2.grid()
         label4['text'] = 'Login successful'
         root.after(10, root.grid)
-        f1.pack_forget()
+        f1.grid_remove()
         root.after(10, root.grid)
-        f3.pack()
+        f3.grid()
         label9.grid_remove()
         root.after(10, root.grid)
 
 def hide_login_label():
-    f2.pack_forget()
-    f3.pack_forget()
+    f2.grid_remove()
+    f3.grid_remove()
     root.after(10, root.grid)
 
 def send_mail():
@@ -53,12 +53,12 @@ def send_mail():
 def logout():
     try:
         server.quit()
-        f3.pack_forget()
+        f3.grid_remove()
         f2.pack()
         label4.grid()
         label4['text'] = 'Logged Out successful'
         btn2.grid_remove()
-        f1.pack()
+        f1.grid()
         entry2.delete(0, END)
         root.after(10, root.grid)
     except Exception as e:
@@ -68,16 +68,16 @@ def validate_login():
     email_text = str(entry1.get())
     pass_text = str(entry2.get())
     if (email_text == '' or pass_text == ''):
-        f2.pack()
+        f2.grid()
         label4.grid()
         label4['text'] = 'Please fill all required fields'
         btn2.grid_remove()
         root.after(10, root.grid)
         return False
     else:
-        EMAIL_REGEX = re.compile(r"[ˆ@\s]+@[ˆ@\s]+\.[a-zA-Z0-9]+$")
+        EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
         if not EMAIL_REGEX.match(email_text):
-            f2.pack()
+            f2.grid()
             label4.grid()
             label4['text'] = 'Enter a valid email address'
             btn2.grid_remove()
@@ -114,58 +114,67 @@ root = Tk()
 root.title("Email Application")
 
 # Frame 1
-f1 = Frame(root, width=1000 , height=800)
-f1.pack(side=TOP)
+f1 = Frame(root)
+f1.grid(row=0, column=0)
 
-label1 = Label(width=25, text='Enter your credentials', font=('calibri', 18, 'bold'))
-label1.grid(row=0, columnspan=3, pady=10, padx=10)
+label1 = Label(f1, width=25, text='Enter your credentials', font=('calibri', 18, 'bold'))
+label1.grid(row=0, column=0, columnspan=3, pady=10, padx=10)
 
-label2 = Label(f1, text='Email').grid(row=1, sticky=E, padx=5, pady=10)
-label3 = Label(f1, text='Password').grid(row=2, sticky=E, padx=5, pady=10)
+label2 = Label(f1, text='Email')
+label2.grid(row=1, column=0, sticky=E, padx=5, pady=10)
+
+label3 = Label(f1, text='Password')
+label3.grid(row=2, column=0, sticky=E, padx=5, pady=10)
 
 entry1 = Entry(f1)
-entry2 = Entry(f1, show='*')
-
 entry1.grid(row=1, column=1, pady=5)
+
+entry2 = Entry(f1, show='*')
 entry2.grid(row=2, column=1)
 
 btn1 = Button(f1, text="Login", width=10, bg='black', fg='white', command=lambda: login())
-btn1.grid(row=3, columnspan=3, pady=10)
+btn1.grid(row=3, column=0, columnspan=3, pady=10)
 
 # Frame 2
 f2 = Frame(root)
-f2.pack(side=TOP, expand=NO, fill=NONE)
+f2.grid(row=1, column=0)
 
 label4 = Label(f2, width=20, text='Login successful', bg='light green', fg='white', font=('calibri', 12, 'bold'))
 label4.grid(row=0, column=0, columnspan=2,  pady=5)
 
 btn2 = Button(f2, text="X", bg="red", fg="white", command=lambda: logout())
-btn2.grid(row=0, column=4, sticky=E, pady=10, padx=(5, 0))
+btn2.grid(row=0, column=2, sticky=E, pady=10, padx=(5, 0))
 
 # Frame 3
 f3 = Frame(master=root)
-f3.pack(side=TOP, expand=NO, fill=NONE)
+f3.grid(row=2, column=0)
 
 label5 = Label(f3, width=20, text='Compose email', font=('calibri', 18, 'bold'))
-label5.grid(row=0, columnspan=3, pady=10)
+label5.grid(row=0, column=0, columnspan=3, pady=10)
 
-label6 = Label(f3, text='To').grid(row=1, sticky=E, pady=5)
-label7 = Label(f3, text='Subject').grid(row=2, sticky=E, pady=5)
-label8 = Label(f3, text='Message').grid(row=3, sticky=E)
+label6 = Label(f3, text='To')
+label6.grid(row=1, column=0, sticky=E, pady=5)
+
+label7 = Label(f3, text='Subject')
+label7.grid(row=2, column=0, sticky=E, pady=5)
+
+label8 = Label(f3, text='Message')
+label8.grid(row=3, column=0, sticky=E)
 
 entry3 = Entry(f3)
-entry4 = Entry(f3)
-entry5 = Entry(f3)
-
 entry3.grid(row=1, column=1, pady=5)
+
+entry4 = Entry(f3)
 entry4.grid(row=2, column=1, pady=5)
+
+entry5 = Entry(f3)
 entry5.grid(row=3, column=1, pady=5, rowspan=3, ipady=10)
 
 btn3 = Button(f3, text="Send", width=10, bg='black', fg='white', command=lambda: send_mail())
-btn3.grid(row=6, columnspan=3, pady=10)
+btn3.grid(row=6, column=0, columnspan=3, pady=10)
 
 label9 = Label(f3, width=20, bg='light green', fg='white', font=('calibri', 18, 'bold'))
-label9.grid(row=7, columnspan=3, pady=5)
+label9.grid(row=7, column=0, columnspan=3, pady=5)
 
 hide_login_label()
 
