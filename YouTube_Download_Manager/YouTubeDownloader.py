7417fe20ef6f8aba632a8b3b1eec757c6ba1b682
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-02-26 15:53:31 +0100

    #3 select directory

--- YouTube_Download_Manager/YouTubeDownloader.py
+++ YouTube_Download_Manager/YouTubeDownloader.py
@@ -23,6 +23,30 @@ class Application:
         self.youtubeEntry = Entry(self.root, width=70, textvariable=self.YouTubeEntryVar, font=('Agenta', 25, 'italic'), fg='red', bg="#CCFFF7")
         self.youtubeEntry.grid(pady=(0, 15), ipady=2)
 
+        self.youtubeEntryError = Label(self.root, text="", font=("Concert One", 20), bg="#CCFFF7", fg="red")
+        self.youtubeEntryError.grid(pady=(0, 8))
+
+        self.youtubeFileSaveLabel = Label(self.root, text='Choose directory', font=('Concert One', 30, 'bold'), fg='black', bg="#CCFFF7")
+        self.youtubeFileSaveLabel.grid()
+
+        self.youtubeFileDeirectoryBtn = Button(self.root, text='Directory', font=('Bell MT', 15), command=self.openDirectory)
+        self.youtubeFileDeirectoryBtn.grid(pady=(10, 3))
+
+        self.youtubeFileSaveLabel = Label(self.root, text='', font=('Franklin Gothic', 25,), bg="#CCFFF7")
+        self.youtubeFileSaveLabel.grid()
+
+    def openDirectory(self):
+        self.FolderName = filedialog.askdirectory()
+
+        if len(self.FolderName) > 0:
+            self.youtubeFileSaveLabel.config(text=self.FolderName,fg="green")
+            return True
+        else:
+            self.youtubeFileSaveLabel.config(text="Please choose directory",fg="red")
+            
+
+
+
 if __name__=="__main__":
     window = Tk()
     window.title("YouTube Downloader")
