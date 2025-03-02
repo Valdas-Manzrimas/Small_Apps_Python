0298ce59342a9ff6bb208927827bb733f785184a
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-03-02 18:06:13 +0100

    #5 Check_versions function

--- ScreenShot/ScreenShot.py
+++ ScreenShot/ScreenShot.py
@@ -49,6 +49,13 @@ def main(url, w, h):
     print("Opening your screenshot with the default application")
     open_with_default_app(SCREENSHOT_PATH)
 
+def check_versions():
+    ver = cef.GetVersion()
+    print("CEF Python {ver}".format(ver=ver["version"]))
+    print("Chromium {ver}".format(ver=ver["chrome_version"]))
+    print("CEF {ver}".format(ver=ver["cef_version"]))
+    print("Python {ver} {arch}".format(ver=platform.python_version(), arch=platform.architecture()[0]))
+    assert cef.__version__>="57.0", "CEF Python v57.0+ required to run this example"
 
 
 
