086545d14e58abd003d9a5b38f5a8519ac9c79ae
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-03-06 18:36:41 +0100

    #6 Set volume and menu bar

--- Music_Player/Player.py
+++ Music_Player/Player.py
@@ -8,7 +8,16 @@ mixer.init()
 window.geometry("300x300")
 window.title("Music Player")
 
-txt_label = Label(window, text="Music Player", font=("Arial", 20))
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+sub_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=sub_menu)
+sub_menu.add_command(label="Open")
+sub_menu.add_separator()
+sub_menu.add_command(label="Exit", command=window.quit)
+
+txt_label = Label(window, text="Music Player", pady=10, font=("Arial", 20))
 txt_label.pack()
 
 def play_action():
@@ -17,6 +26,10 @@ def play_action():
 
 def stop_action():
     mixer.music.stop()
+
+def set_volume(value):
+    volume = int(value) / 100
+    mixer.music.set_volume(volume)
 
 button_frame = Frame(window)
 button_frame.pack(padx=40)
@@ -28,6 +41,10 @@ play_btn.pack(side=LEFT, padx=(10, 5))
 stop_img = PhotoImage(file='stop.png')
 stop_btn = Button(button_frame, image=stop_img, command=stop_action)
 stop_btn.pack(side=RIGHT, padx=(5, 10))
+
+scale = Scale(window, from_=0, to=100, orient=HORIZONTAL, command=set_volume)
+scale.set(70)
+scale.pack()
 
  
 
