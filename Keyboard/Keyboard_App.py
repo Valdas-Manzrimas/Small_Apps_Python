f1765d057852b878d725bd0660e89ab131f6b704
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-03-11 15:53:47 +0100

    #8 Keybord GUI

--- Keyboard/Keyboard_App.py
+++ Keyboard/Keyboard_App.py
@@ -11,10 +11,35 @@ keys = [
     "SPACE"]
 
 curBtn = [-1, -1]
-button = [[]]
+buttonL = [[]]
 entry = tk.Text(Keyboard, width=97, height=8)
 entry.grid(row=0, columnspan=15)
 
 varRow = 1
 varColumn = 0
 
+for button in keys:
+    if button != "SPACE":
+        btn = tk.Button(Keyboard, text=button, width=5, bg='black', fg='white', highlightthickness=4, activebackground='gray', activeforeground='red', highlightcolor='red', relief='raised', padx=12, pady=4, bd=4, command=lambda x=button, i=varRow-1, j=varColumn: selelct(x, i, j))
+
+        btn.bind('<<Return>>', lambda event, x=button, i=varRow-1, j=varColumn: selelct(x, i, j))
+        buttonL[varRow-1].append(btn) 
+        btn.grid(row=varRow, column=varColumn)
+
+    if button == "SPACE":
+        btn = tk.Button(Keyboard, text=button, width=60, bg='black', fg='white', highlightthickness=4, activebackground='gray65', activeforeground='red', highlightcolor='red', relief='raised', padx=4, pady=4, bd=4, command=lambda x=button, i=varRow-1, j=varColumn: selelct(x, i, j))
+
+        btn.bind('<<Return>>', lambda event, x=button, i=varRow-1, j=varColumn: selelct(x, i, j))
+        buttonL[varRow-1].append(btn) 
+        btn.grid(row=6, columnspan=16)
+
+    varColumn += 1
+    if varColumn > 10:
+        varColumn = 0
+        varRow += 1
+        buttonL.append([])
+
+
+
+Keyboard.mainloop()
+                        
