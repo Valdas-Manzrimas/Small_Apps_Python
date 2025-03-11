4687c2c40470b6fe233e3d47718a745c82c35a85
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-03-11 14:10:23 +0100

    #7 createWidgets f() created. Dropdown btns added to select movie and genre

--- Booking/Booking.py
+++ Booking/Booking.py
@@ -15,3 +15,42 @@ times = ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
 
 seatList = []
 seatSelected = []
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Cinema Booking")
+        self.createWidgets()
+    
+    def updateMovies(self, event=None):
+        self.movieCombo['values'] = movies[self.genreCombo.get()]
+
+    def createWidgets(self):
+        headingLabel = tk.Label(self, text="Cinema Booking", font=("Aries 12 bold"))
+        headingLabel.grid(row=0, column=0, columnspan=5, padx=10, pady=10, sticky="w")
+        tkinter.ttk.Separator(self, orient="horizontal").grid(row=1, column=0, columnspan=5, sticky="w")
+
+        day = tk.Frame(self)
+        tk.Label(day, text="_____", ).pack()
+        tk.Label(day, text="Today", font=("Aries 10 underline")).pack()
+        tk.Label(day, text="").pack()
+        day.grid(row=2, column=0, padx=10)
+
+        tk.Label(self, text="Genre: ").grid(row=2, column=1, padx=(10, 0))
+        self.genreCombo = tkinter.ttk.Combobox(self, width=15, values=list(movies.keys()), state="readonly")
+
+        self.genreCombo.set("Select Genre")
+        self.genreCombo.bind('<<ComboboxSelected>>', self.updateMovies)
+        self.genreCombo.grid(row=2, column=2)
+
+        tk.Label(self, text="Movie: ").grid(row=2, column=3, padx=(10, 0))
+        self.movieCombo = tkinter.ttk.Combobox(self, width=15, state="readonly")
+        self.movieCombo.bind('<<ComboboxSelected>>', self.createTimeButtons)
+        self.movieCombo.set("Select Movie")
+        self.movieCombo.grid(row=2, column=4, padx=(10, 0))
+        tkinter.ttk.Separator(self, orient="horizontal").grid(row=3, column=0, columnspan=5, sticky="w")
+
+
+app = Application()
+
+app.mainloop()
