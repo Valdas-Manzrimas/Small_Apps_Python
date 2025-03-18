4d952a26ad2e2db2a2c3ec40195a3449992ad0be
Valdas Manzrimas <vrajaviharidas@gmail.com>
2025-03-18 18:23:08 +0100

    #11 Board created

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -13,7 +13,17 @@ class tictactoe(tk.Tk):
 
         self.Board()
 
-        
+    def Board(self):
+        for i in range(0, 3):
+            row = []
+            for j in range(0, 3):
+                row.append(tk.Button(self, width=10, height=3, font=("Calibri", 35, "bold"), bg="white", command=lambda x=i, y=j: self.Turn_Taken(x, y)))
+
+                row[j].grid(row=i, column=j)
+            self.btns.append(row) 
+        tk.Button(self, text="Restart", width=10, height=1, font=("Calibri", 15, "bold"), bg="black", fg="white", activebackground="grey", activeforeground="green", command=self.NewGame).grid(row=3, column=1)
+
+    
 
 
 
